9605e6255d8dace25c6c3cc50006e5a8651bdb8f
Vijay <yarrarapuvijay@gmail.com>
2026-07-21 09:55:58 +0530

    script.js

--- script.js
+++ script.js
@@ -1,28 +1,13 @@
 const ui = document.getElementById("ui");
+const totalItems = 67;
 
 function createAnimation() {
-
     ui.innerHTML = "";
 
     const scene = Math.min(window.innerWidth * 0.92, 450);
-
-    // Automatically decide how many words to create
-    let totalItems;
-
-    if (scene < 280) {
-        totalItems = 36;
-    } else if (scene < 340) {
-        totalItems = 45;
-    } else if (scene < 400) {
-        totalItems = 56;
-    } else {
-        totalItems = 67;
-    }
-
     document.documentElement.style.setProperty("--scene", `${scene}px`);
 
     for (let i = 1; i <= totalItems; i++) {
-
         const love = document.createElement("div");
         love.className = "love";
         love.style.setProperty("--i", i);
@@ -41,14 +26,4 @@ function createAnimation() {
 
 createAnimation();
 
-let resizeTimer;
-
-window.addEventListener("resize", () => {
-
-    clearTimeout(resizeTimer);
-
-    resizeTimer = setTimeout(() => {
-        createAnimation();
-    }, 200);
-
-});
+window.addEventListener("resize", createAnimation);
